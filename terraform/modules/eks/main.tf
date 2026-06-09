524fa40c30c3f203bf5f4fbc824497b207007560
faqsarg <sargiottofacu@gmail.com>
2026-06-09 08:53:43 -0300

    {feat/eks}: IRSA via OIDC provider + module outputs
    - registered cluster OIDC issuer in IAM (aws_iam_openid_connect_provider)
    - dynamic thumbprint via tls_certificate data source (not hardcoded)
    - module outputs: cluster_name, endpoint, ca, oidc issuer url + provider arn

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -76,3 +76,13 @@ resource "aws_eks_node_group" "this" {
 
   depends_on = [aws_iam_role_policy_attachment.node]
 }
+
+data "tls_certificate" "oidc" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "this" {
+  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.oidc.certificates[0].sha1_fingerprint]
+}
